6fba6ad4ccb85d85e1e902fced29e4b44233b87d
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-03-11 11:38:50 +0100

    Add project files.

---
+++ EnResaGenomASP/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EnResaGenomASP
+{//Här startar programmet - Main
+    public class Program
+    {
+        public static void Main(string[] args)     
+        {
+            //loggar en strin i vs output konsollen
+            Debug.WriteLine("HELLO, lets start debug!");
+            //skriver ut en string i konsollen som startar upp
+            Console.WriteLine("HELLO, lets start!");
+
+            //här skriver jag om creatHostBuilder så allt sker i Main-metoden istället.
+
+            //jag skapar en variabel: HotstBuilder och lägger DefaultBuilder där i. 
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+            //jag kallar på ConfigureWebHostDefaults, och sparar ner resultatet i webHost. 
+            hostBuilder.ConfigureWebHostDefaults(webBuilder =>
+            {//här berättar vi att att man ska använda startup-classen och dess metoder. 
+                webBuilder.UseStartup<Startup>();
+            });
+            //Jag kallar på Build() i variabeln webHost
+            IHost webHost = hostBuilder.Build();
+            //i webHost kallar jag sedan på Run();
+            webHost.Run();          
+        }
+    
+    }
+}
---
+++ EnResaGenomASP/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+
+namespace EnResaGenomASP
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            //här får jag tag på mitt värde och skriver ut värdet i output, 
+            Configuration = configuration;
+            Debug.WriteLine(Configuration["MyProperty"]);
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
